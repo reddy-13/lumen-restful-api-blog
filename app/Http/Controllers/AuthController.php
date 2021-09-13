0dfbfc711ef7d30666a479f0f04f0278502c3a8d
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2021-09-13 21:35:50 +0530

    Update AuthController.php
    Register user fixed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\JWTAuth;
@@ -21,11 +22,41 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         # Register new user
-        $this->validate($request, [
-            'name' => 'required',
-            'email'    => 'required|email|max:255',
-            'password' => 'required',
-        ]);
+        $name = $request->name;
+        $email = $request->email;
+        $password = $request->password;
+
+        // Check if field is not empty
+        if (empty($name) or empty($email) or empty($password)) {
+            return response()->json(['status' => 'error', 'message' => 'You must fill all the fields']);
+        }
+
+        // Check if email is valid
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return response()->json(['status' => 'error', 'message' => 'You must enter a valid email']);
+        }
+
+        // Check if password is greater than 5 character
+        if (strlen($password) < 6) {
+            return response()->json(['status' => 'error', 'message' => 'Password should be min 6 character']);
+        }
+
+        // Check if user already exist
+        if (User::where('email', '=', $email)->exists()) {
+            return response()->json(['status' => 'error', 'message' => 'User already exists with this email']);
+        }
+        try {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = app('hash')->make($request->password);
+            if ($user->save()) {
+                // Will call login method
+                return $this->login($request);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()]);
+        }
     }
 
     public function login(Request $request)
